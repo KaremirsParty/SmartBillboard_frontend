26bfe14dc827f187f5cef582466953fd3b340f1b
Aleksander <aleksanderp1993@gmail.com>
2021-10-28 09:51:56 +0200

    still not working ;(

--- SmartBillboard-frontend/custom-webpack.config.js
+++ SmartBillboard-frontend/custom-webpack.config.js
@@ -10,12 +10,12 @@ module.export = {
         "tls": false,
         "net": false,
         "zlib": false,
-        "os": require.resolve("os-browserify/browser"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "stream": require.resolve("stream-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify 
+        "os": false,
+        "http": false,
+        "https": false,
+        "stream": false,
+        "crypto": false,
+        "crypto-browserify": false, //if you want to use this module also don't forget npm i crypto-browserify 
       } 
     }
   }
